6e1836427cb95aa82dc7085bb5a87da24cc6f7ff
stormwasd <2565706797@qq.com>
2023-02-10 07:41:24 +0800

    router-test

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,13 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login', async (ctx, next) => {
+  const {userName, password } = ctx.reqrest.body
+  // 登录验证的逻辑
+  ctx.body = {
+    userName,
+    password
+  }
+})
+
 module.exports = router
