88873e4af6e7529b14105b256e3db4f2f26f37ea
stormwasd <2565706797@qq.com>
2023-02-13 21:33:13 +0800

    add cache

---
+++ src/cache/_redis.js
@@ -0,0 +1,61 @@
+/**
+ * @description 连接 redis的方法，get set
+ * @author JackLiLi
+ */
+
+const redis = require('redis');
+const { REDIS_CONF } = require('../conf/db');
+
+// 创建客户端
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host);
+redisClient.on('error', err => {
+    console.error('redis error', err)
+});
+
+// set
+/**
+ * redis set
+ * @param key 键
+ * @param val 值
+ * @param timeout 过期时间 单位 s
+ */
+function set(key, val, timeout = 60 * 60) {
+    if (typeof val === 'object') {
+        val = JSON.stringify(val)
+    }
+    redisClient.set(key, val);
+    redisClient.expire(key, timeout)
+}
+
+// get
+/**
+ * redis get
+ * @param key 键
+ */
+function get(key) {
+    const promise = new Promise((resolve, reject) => {
+        redisClient.get(key, (err, val) => {
+            if (err) {
+                reject(err);
+                return
+            }
+            if (val == null) {
+                resolve(null);
+                return
+            }
+            try {
+                resolve(
+                    JSON.parse(val)
+                )
+            } catch (ex) {
+                resolve(val)
+            }
+        })
+    });
+    return promise
+}
+
+module.exports = {
+    set,
+    get
+};
---
+++ src/conf/db.js
@@ -0,0 +1,23 @@
+/**
+ * @description 存储配置
+ * @author JackLiLi
+ */
+
+const { isProd } = require('../utils/env');
+
+let REDIS_CONF = {
+  port: 6379,
+  host: '127.0.0.1'
+};
+
+if (isProd) {
+    REDIS_CONF = {
+        // 线上的redis配置
+        port: 6379,
+        host: '127.0.0.1'
+    }
+}
+
+module.exports = {
+  REDIS_CONF
+};
