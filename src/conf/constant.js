7de422119d5752d091e93863f50b33ac6c100ef5
stormwasd <2565706797@qq.com>
2023-02-19 15:51:47 +0800

    feat: 增加 setting 页面模板

--- src/conf/constant.js
+++ src/conf/constant.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DEFAULT_PICTURE: 'https://dwz.cn/rnTnftZs'
+    DEFAULT_PICTURE: 'https://cdn.staticaly.com/gh/stormwasd/image-hosting@master/20230218/image.5z0riejiq1c0.webp'
 }
