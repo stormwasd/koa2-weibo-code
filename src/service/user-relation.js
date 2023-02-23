d3bc864634fcef55fcab3eddef578f3cc3a1b96c
stormwasd <2565706797@qq.com>
2023-02-23 18:42:54 +0800

    comments

--- src/service/user-relation.js
+++ src/service/user-relation.js
@@ -12,6 +12,10 @@ const Sequelize = require('sequelize')
  * @param {number} followerId 被关注人的 id
  */
 async function getUsersByFollower(followerId) {
+    // 通过Follower拿到所有的User(粉丝)
+    /**
+     * 所以首先传入一个followerId，在UserRelation中查询，然后拿到userId关联到User表，并拿到'id', 'userName', 'nickName', 'picture'
+     */
     const result = await User.findAndCountAll({
         attributes: ['id', 'userName', 'nickName', 'picture'],
         order: [
@@ -47,6 +51,10 @@ async function getUsersByFollower(followerId) {
  * @param {number} userId userId
  */
 async function getFollowersByUser(userId) {
+    // 通过User拿到所有的Follower(关注的人)
+    /**
+     * 所以首先传入一个userId在UserRelation表中查询，查询到该userId对应的所有followerId，然后再通过外键followerId去User表中拿到对应的用户信息
+     */
     const result = await UserRelation.findAndCountAll({  // UserRelation这个模型做查询，首先where这个查询就是对于UserRelation这个模型中把userId等于传入的userId的记录拿出来
         // 续上，通过userId把followerId查询出来，然后我们还要通过followerId找到我关注人的信息，正好include了User这个模型，为什么这个地方就能通过followerId找到用户信息呢？因为我们通过
         // 续上，UserRelation去find，同时include了User的时候，正好就命中了UserRelation.belongsTo(User, { foreignKey: 'followerId' })
